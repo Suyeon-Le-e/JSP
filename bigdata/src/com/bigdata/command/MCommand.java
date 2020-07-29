16c0f708ed7807487bdea874e10529f7872c844a
Suyeon-Le-e <66981071+Suyeon-Le-e@users.noreply.github.com>
2020-07-29 15:31:20 +0900

    COMMIT

--- bigdata/src/com/bigdata/command/MCommand.java
+++
@@ -1,8 +0,0 @@
-package com.bigdata.command;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface MCommand {
-	void execute(HttpServletRequest request, HttpServletResponse response);
-}
